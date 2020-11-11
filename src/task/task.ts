495463880b819ea327a28c5a921986d15feac9ab
Nikita Khakimov <53759907+grzdrz@users.noreply.github.com>
2020-11-12 04:14:43 +0700

    добавил свг

--- src/task/task.ts
+++ src/task/task.ts
@@ -1,40 +1,74 @@
 import Vector from '../assets/Vector';
 import CanvasManager from './canvas-manager';
+import constants from './constants';
+
+import './task.scss';
 
 class Task {
   public container: HTMLElement;
   public canvasManager: CanvasManager;
+  public mapContainer: HTMLElement;
+  public countries: SVGPathElement[];
+  public svg: SVGSVGElement;
+
+  /* public baseWidth: number;
+  public baseHeight: number; */
 
   constructor(container: HTMLElement) {
     this.container = container;
 
     this.initialize();
     this.setEventsHandlers();
+    /* this.setSize(); */
   }
 
   initialize(): void {
-    const canvas = this.container.querySelector('canvas');
-    this.canvasManager = new CanvasManager(canvas);
+    /* const canvas = this.container.querySelector('canvas');
+    this.canvasManager = new CanvasManager(canvas); */
+
+    this.svg = this.container.querySelector('svg');
+    this.countries = [...this.container.querySelectorAll('path')];
+    this.countries.forEach((path) => {
+      path.style.fill = 'rgb(78, 92, 124)';
+    });
   }
 
   setEventsHandlers(): void {
-    this.canvasManager.canvas.ondragstart = () => false;
+    /* this.canvasManager.canvas.ondragstart = () => false;
     this.canvasManager.canvas.addEventListener('mousedown', this.handleMouseDown);
-    this.canvasManager.canvas.addEventListener('touchstart', this.handleMouseDown);
+    this.canvasManager.canvas.addEventListener('touchstart', this.handleMouseDown); */
+
+    this.countries.forEach((path) => {
+      path.addEventListener('click', this.handlePathClick);
+    });
   }
 
+  setSize(): void {
+    const { kx, ky } = this.calculateScaleCoefficient(this.mapContainer.clientWidth, this.mapContainer.clientHeight);
+    this.svg.style.transform = `scale(${kx}, ${kx})`;
+    this.countries.forEach((path) => {
+      path.style.transform = `scale(${kx}, ${kx})`;
+    });
+  }
+
+  calculateScaleCoefficient(width: number, height: number): { kx: number, ky: number } {
+    const kx = width / this.svg.clientWidth;
+    const ky = height / this.svg.clientHeight;
+    return { kx, ky };
+  }
+
+  handlePathClick = (event: Event): void => {
+    this.countries.forEach((path) => {
+      path.style.fill = 'rgb(78, 92, 124)';
+      /* path.style.transform = 'scale(1, 1)'; */
+    });
+
+    const target = <HTMLElement>event.target;
+    target.style.fill = 'red';
+    /* target.style.transform = 'scale(1.2, 1.2)'; */
+  };
+
   handleMouseDown = (event: UIEvent) => {
-    /* const optionsForMouseEvents = {
-      handleMouseMove: (event) => { },
-      handleMouseUp: (event) => { },
-      mousePositionInsideHandle,
-    };
-    const handleMouseMove = handleMouseMove.bind(this, optionsForMouseEvents);
-    optionsForMouseEvents.handleMouseMove = handleMouseMove;
-
-    const handleMouseUp = handleMouseUp.bind(this, optionsForMouseEvents);
-    optionsForMouseEvents.handleMouseUp = handleMouseUp; */
-
     document.addEventListener('mousemove', this.handleMouseMove);
     document.addEventListener('mouseup', this.handleMouseUp);
     document.addEventListener('touchmove', this.handleMouseMove);
@@ -68,9 +102,6 @@ class Task {
       x = event.clientX;
       y = event.clientY;
     }
-    // y = (document.documentElement.clientHeight + window.pageYOffset) - y;
-
-    /* return new Vector(x, y); */
     return new Vector(x, y);
   }
 }
